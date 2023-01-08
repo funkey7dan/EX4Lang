5b439e9a60d91ee2586005190800be6519fdb206
Ben <benelashvlili@gmail.com>
2023-01-08 20:00:00 +0200

    -

---
+++ goldbach.pl
@@ -0,0 +1,11 @@
+is_mod(A,B) :- not(mod(A,B) =:= 0).
+is_prime_helper(A,B) :- B*B>A;(is_mod(A,B),B1 is (B+2),is_prime_helper(A,B1)).
+is_prime(2).
+is_prime(A) :- A>1,(is_mod(A,2)),is_prime_helper(A,3).
+
+goldbach(N, []) :- N < 2;not(mod(N,2) =:= 0).
+goldbach(N, [H|T]) :-
+    between(3, N, H),
+    T is N - H,
+    is_prime(H),
+    is_prime(T).
