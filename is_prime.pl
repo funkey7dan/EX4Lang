d75b03def82babb50463e7fd36d1bec171ca368d
Ben <benelashvlili@gmail.com>
2023-01-08 18:11:32 +0200

    is_prime completed - log n time

--- is_prime.pl
+++ is_prime.pl
@@ -1,20 +1,4 @@
-gcd(X, X, X). 
-gcd(X, Y, D) :- 
-    X < Y, 
-    Y1 is Y - X, 
-    gcd(X, Y1, D). 
-gcd(X, Y, D) :- 
-    X > Y, 
-    gcd(Y, X, D).
-
-is_mod(A,B,C) :- A mod B = C.
-
-is_prime(0).
-is_prime(1).
+is_mod(A,B) :- not(mod(A,B) =:= 0).
+is_prime_helper(A,B) :- B*B>A;(is_mod(A,B),B1 is (B+2),is_prime_helper(A,B1)).
 is_prime(2).
-is_prime(3).
-is_prime(5).
-is_prime(7).
-%is_prime(A) :- D = 1, gcd(A,A1,D), A is A1 + 1.
-is_prime(A) :- is_mod(A,A1,1),A1 is A-1,is_prime(A1).
-is_prime(A) :- false.
+is_prime(A) :- A>1,(is_mod(A,2)),is_prime_helper(A,3).
